517fd9b3c7fb90f6985ecb1cf3a9c22a97de22d5
C-FamilyDeveloper <vladmakushevvlad@yandex.ru>
2023-09-14 19:59:56 +0300

    Added new API controllers

---
+++ WorkMonitorServer/Controllers/LogsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorkMonitorServer.Models.DataContexts;
+using WorkMonitorServer.Models.DataEntities;
+
+namespace WorkMonitorServer.Controllers
+{
+    [ApiController]
+    [Route("api/logs")]
+    public class LogsController : Controller
+    {
+        private ILogger<LogsController> logger;
+        private readonly ApplicationContext applicationContext;
+
+        public LogsController(ILogger<LogsController> logger, ApplicationContext applicationContext)
+        {
+            this.logger = logger;
+            this.applicationContext = applicationContext;
+        }
+        [HttpPost]
+        public async Task Post(WorkMonitorTypes.Requests.Log log)
+        {
+            Client? client = await applicationContext.Clients.Where(i => i.Name == log.ClientName).FirstOrDefaultAsync();
+            if (client == default)
+            {
+                BadRequest();
+            }
+            await applicationContext.Logs.AddAsync(new Models.DataEntities.Log 
+            { 
+                LogDateTime = log.LogDateTime,
+                LogMessage = log.LogMessage,
+                Client = client!
+            });
+        }
+    }
+}
